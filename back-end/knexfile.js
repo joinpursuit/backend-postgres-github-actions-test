a5234b947ab3f6fe0683d7917e3e3382bfb31633
bwreid <bwreid@gmail.com>
2022-05-31 15:04:14 -0700

    Update Knexfile fnd scripts or Heroku deployment

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -26,7 +26,10 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
     migrations: {
       directory: path.join(__dirname, "db", "migrations"),
     },
